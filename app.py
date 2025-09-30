c6b1d0b631a5a86cd73eae31cf50aa21475fe1bb
Hala Arar <halaosama92@yahoo.com>
2025-09-30 15:16:06 -0400

    feat: add --url and --question required flags

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--brand", required=True)
+parser.add_argument("--url", required=True)
+parser.add_argument("--question", required=True)
 args = parser.parse_args()
 
-print(args.brand)
+print("brand:", args.brand)
+print("url:", args.url)
+print("question:", args.question)
